4ca260171faf04a7cb14ad78868c27086126ba75
mxhsb <mxcoder@yandex.com>
2018-10-31 23:05:10 -0300

    create models

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -7,6 +7,5 @@ class Order(models.Model):
     toppings = models.CharField(max_length=60)
     additions = models.CharField(max_length=60)
 
-
-
-
+    def __str__(self):
+        return f"{self.size} {self.type} pizza with {self.toppings} and {self.additions}"
