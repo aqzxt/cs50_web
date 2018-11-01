37f04ef516e95a4a88da9085bfe83a6cfd6f2171
mxhsb <mxcoder@yandex.com>
2018-11-01 02:06:50 -0300

    add index.html

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Every item
 class Order(models.Model):
     type = models.CharField(max_length=60)
     size = models.CharField(max_length=60)
-    toppings = models.CharField(max_length=60)
-    additions = models.CharField(max_length=60)
+    toppings = models.CharField(max_length=60, default="")
+    additions = models.CharField(max_length=60, default="")
 
     def __str__(self):
-        return f"{self.size} {self.type} pizza with {self.toppings} and {self.additions}"
+        if self.toppings and self.additions:
+            return f"{self.size} {self.type} pizza with {self.toppings} and {self.additions}"
+        if self.toppings:
+            return f"{self.size} {self.type} pizza with {self.toppings}"
+        else:
+            return f"{self.size} {self.type} pizza and {self.additions}"
